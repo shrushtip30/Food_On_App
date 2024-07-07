62319b8170071c67f3b2f929c84042cb550883f7
shrushtip30 <patilshrushti93@gmail.com>
2024-07-07 13:19:00 +0530

    FoodOn_App

---
+++ FoodOn_App/app/src/main/java/com/food_on/app/Delivery_FoodPanelBottomNavigation.java
@@ -0,0 +1,87 @@
+package com.food_on.app;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.food_on.app.DeliveryFoodPanel.DeliveryPendingOrderFragment;
+import com.food_on.app.DeliveryFoodPanel.DeliveryShipOrderFragment;
+import com.food_on.app.SendNotification.Token;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class Delivery_FoodPanelBottomNavigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery__food_panel_bottom_navigation);
+        BottomNavigationView navigationView = findViewById(R.id.delivery_bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+        UpdateToken();
+        String name = getIntent().getStringExtra("PAGE");
+        if (name != null) {
+            if (name.equalsIgnoreCase("DeliveryOrderpage"))
+            {
+                loaddeliveryfragment(new DeliveryPendingOrderFragment());
+            }
+
+        } else {
+            loaddeliveryfragment(new DeliveryPendingOrderFragment());
+        }
+
+    }
+
+    private void UpdateToken() {
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull Task<String> task) {
+                if(task.isComplete()){
+                    String token = task.getResult();
+                    FirebaseDatabase.getInstance().getReference("Tokens").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(token);
+
+
+
+                }
+            }
+        });
+   //     String refreshToken = FirebaseInstanceId.getInstance().getToken();
+   //     Token token = new Token(refreshToken);
+      //  FirebaseDatabase.getInstance().getReference("Tokens").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(token);
+    }
+
+    private boolean loaddeliveryfragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.pendingorders:
+                fragment = new DeliveryPendingOrderFragment();
+                break;
+
+            case R.id.shiporders:
+                fragment = new DeliveryShipOrderFragment();
+                break;
+
+        }
+        return loaddeliveryfragment(fragment);
+    }
+}
